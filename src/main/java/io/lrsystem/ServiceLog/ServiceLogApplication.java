eff0c3fb9570bdd1bb48927806fdfa97ae78fdfd
Lucas Rodrigues de Oliveira <lucas-oliveira25@outlook.com>
2025-12-06 20:49:57 -0300

    Subindo projeto

---
+++ src/main/java/io/lrsystem/ServiceLog/ServiceLogApplication.java
@@ -0,0 +1,13 @@
+package io.lrsystem.ServiceLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLogApplication.class, args);
+	}
+
+}
